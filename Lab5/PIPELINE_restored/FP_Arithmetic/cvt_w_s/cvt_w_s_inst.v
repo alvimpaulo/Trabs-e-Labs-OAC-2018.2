c5fc4a22af89acdd250bd1de9b809e4dfba4aed5
Paulo Alvim <pauloalvim@gmail.com>
2018-12-05 16:09:38 -0200

    salvo o arquivo

--- Lab5/PIPELINE_restored/FP_Arithmetic/cvt_w_s/cvt_w_s_inst.v
+++
@@ -1,8 +0,0 @@
-cvt_w_s	cvt_w_s_inst (
-	.clock ( clock_sig ),
-	.dataa ( dataa_sig ),
-	.nan ( nan_sig ),
-	.overflow ( overflow_sig ),
-	.result ( result_sig ),
-	.underflow ( underflow_sig )
-	);
