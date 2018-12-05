4c8b47190feaab6ee3c8a037d3c5d0acd99d356f
Paulo Alvim <pauloalvim@gmail.com>
2018-12-05 15:55:40 -0200

    personagem se movendo

--- README.md
+++
@@ -1,2 +0,0 @@
-# Trabs-e-Labs-OAC-2018.2
-Repositório para os trabalhos e laboratórios da matéria de Organização e Arquitetura de Computadores - Semestre 2018.2
--- pitfall/utilidades.s
+++ pitfall/utilidades.s
@@ -1,5 +1,5 @@
 .text
-## a0 = x_0; a1 = y_0; a2 = x_1; a3 = y_1;
+## a0 = x_0; a1 = y_0; a2 = x_1; a3 = y_1; a4 = y_1;
 DrawQuadrado: nop
 	li t0 INIT_MEMORIA_VIDEO	# carrega a memória inicial de video
 	li t1 320					# carrega a largura da tela
@@ -16,9 +16,8 @@ DrawQuadrado: nop
 	mul t6 a3 t1 #fim
 	add t6 t6 a2 #fim
 	add t6 t6 t0 #fim
-	li a0 0xAA
 	loop_cria_quadrado_linha_DrawQuadrado: nop
-		sb a0 0(t5)								# salva a cor do do quadrado a ser desenhado
+		sb a4 0(t5)								# salva a cor do do quadrado a ser desenhado
 		if_vai_proxima_linha_loop_cria_quadrado_linha_DrawQuadrado: blt t4 t2 else_vai_proxima_linha_loop_cria_quadrado_linha_DrawQuadrado # if es
 			li t4 0
 			add t5 t5 t3
@@ -28,7 +27,7 @@ DrawQuadrado: nop
 		addi t4 t4 1 
 		bne t5 t6 loop_cria_quadrado_linha_DrawQuadrado
 	end_loop_cria_quadrado_linha_DrawQuadrado:
-	sb a0 0(t5)
+	sb a4 0(t5)
 FimDrawQuadrado: jalr x0 ra 0
 
 ## Retorna true se (x, y) est� dentro do quadrado ((x_0, y_0), (x_1, y_1)) e false caso contr�rio
@@ -51,10 +50,12 @@ ColisaoObj: nop
 FimColisaoObj: jalr x0 ra 0
 
 LeTeclaDoTeclado:	nop
+	li a0 0
 	li t1 INIT_MEMORIA_TECLADO # carrega valor da memória inicial do tecla
 	lw a1 ADD_MEMORIA_TECLADO_ESTA_ATIVO(t1) # carrega a word onde tem o bit que diz que uma teca foi apertada
 	andi a1 a1 MASK_MEMORIA_TECLADO # deixa somente o bit que diz que uma teca foi apertada
-  if_tem_tecla_apertada_LeTeclaDoTeclado: beq t0 x0 FimLeTeclaDoTeclado # if nenhuma tecla foi apertada ele cai fora
+  if_tem_tecla_apertada_LeTeclaDoTeclado: beq a1 x0 FimLeTeclaDoTeclado # if nenhuma tecla foi apertada ele cai fora
   	lw a0, ADD_MEMORIA_TECLADO_LE(t1) # carrega o valor da tecla apertada em a0
+  	sw a0, ADD_MEMORIA_TECLADO_MOSTRA(t1) #mostra no display a tecla pressionada
 	else_tem_tecla_apertada_LeTeclaDoTeclado:
 FimLeTeclaDoTeclado: jalr x0 ra 0
